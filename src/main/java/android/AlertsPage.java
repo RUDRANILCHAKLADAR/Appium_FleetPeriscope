6d741c7fa3df04814e1d65c07065cdc7d351dbc9
MDSpireon <104892745+MDSpiron@users.noreply.github.com>
2023-06-21 16:35:28 +0530

    conflict

--- src/main/java/android/AlertsPage.java
+++ src/main/java/android/AlertsPage.java
@@ -1,156 +1,156 @@
-package android;
-
-import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.pagefactory.AndroidFindBy;
-import io.appium.java_client.pagefactory.AppiumFieldDecorator;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.PageFactory;
-
-public class AlertsPage {
-
-    AndroidDriver driver;
-
-    public AlertsPage(AndroidDriver driver) {
-        super();
-        this.driver = driver;
-        PageFactory.initElements(new AppiumFieldDecorator(driver), this);
-    }
-
-
-
-    @AndroidFindBy(xpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.LinearLayout/android.view.ViewGroup/android.widget.TextView")
-     private WebElement Alert_Title_text;
-
-    @AndroidFindBy(id = "com.spireon.fleet.staging:id/action_filter_alerts")
-    private WebElement Filter_icon;
-
-    @AndroidFindBy(id = "com.spireon.fleet.staging:id/rv_filters")
-    private WebElement Total_itemsCount;
-
-    @AndroidFindBy(id = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.LinearLayout/android.widget.RelativeLayout/android.view.ViewGroup[2]/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.RelativeLayout[2]")
-    private WebElement Alert_List;
-
-    @AndroidFindBy(id = "com.android.permissioncontroller:id/permission_allow_foreground_only_button")
-    private WebElement permission_access;
-
-    @AndroidFindBy(id = "com.spireon.fleet.staging:id/action_alerts")
-    private WebElement Alerts_icon;
-
-    @AndroidFindBy(xpath = "(//android.view.View[@content-desc=\"MARKER_GEOZONE.\"])[17]")
-    private WebElement Alert1;
-
-    @AndroidFindBy(xpath = "//android.widget.ImageButton[@content-desc=\"Navigate up\"]")
-    private WebElement Back;
-
-    @AndroidFindBy(xpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.LinearLayout/android.view.ViewGroup[1]/android.widget.TextView")
-    private WebElement Filters_Text;
-
-    @AndroidFindBy(id = "com.spireon.fleet.staging:id/snackbar_text")
-    private WebElement Network_Error_Msg;
-
-    @AndroidFindBy(id = "com.spireon.fleet.staging:id/snackbar_action")
-    private WebElement Retry;
-    @AndroidFindBy(id = "com.spireon.fleet.staging:id/bb_bottom_bar_icon")
-    private WebElement HomeScreen_icon;
-
-    public String safety="Safety (5)";
-    public String prod="Productivity (4)";
-    public String monitor="Monitoring (2)";
-    public String asset="Asset Health (5)";
-
-
-
-
-    public void Click_HomeScreen_icon()
-    {
-         HomeScreen_icon.click();
-    }
-
-    public String safety1="Hard Braking and Hard Acceleration";
-    public String safety2="Odd Hours";
-    public String safety3="Posted Speed Limit";
-    public String safety4="Speed Threshold";
-    public String safety5="Unauthorized Movement";
-
-    public String productivity1="Idle";
-    public String productivity2="Stop";
-    public String productivity3="Input";
-    public String productivity4="Landmark Arrival and Departure";
-
-    public String monitor1="Fuel Tank";
-    public String monitor2="Temperature";
-
-    public String asset1="Low Battery";
-    public String asset2="Maintenance";
-    public String asset3="Tire Pressure";
-    public String asset4="Engine Oil Life";
-    public String asset5="DTC Alert";
-
-
-    public String getNetwork_Error_Msg()
-    {
-       return  Network_Error_Msg.getText();
-    }
-
-    public String network_msg="Please check your network connection and try again.";
-
-    public void ClickRetry()
-    {
-         Retry.click();
-    }
-    public String getFilter_text()
-    {
-        return Filters_Text.getText();
-    }
-
-    public String filter_text="Filters";
-
-    public void ClickBack()
-    {
-        Back.click();
-    }
-    public void ClickAlert1()
-    {
-        Alert1.click();
-    }
-
-    public void ClickAlerts_icon()
-    {
-        Alerts_icon.click();
-    }
-
-
-
-    public String  getAlert_Title_text()
-    {
-        return Alert_Title_text.getText();
-    }
-    public String  Alert_Title="Alerts - 7 Day History";
-
-    public WebElement getFilter_icon()
-    {
-        return Filter_icon;
-    }
-
-    public void Click_filter()
-    {
-        Filter_icon.click();
-    }
-
-    public WebElement getAlert_List()
-    {
-        return Alert_List;
-    }
-
-    public WebElement getTotal_itemsCount()
-    {
-        return Total_itemsCount;
-    }
-
-    public void Click_Permission()
-    {
-        permission_access.click();
-    }
-
-
-}
+//package android;
+//
+//import io.appium.java_client.android.AndroidDriver;
+//import io.appium.java_client.pagefactory.AndroidFindBy;
+//import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+//import org.openqa.selenium.WebElement;
+//import org.openqa.selenium.support.PageFactory;
+//
+//public class AlertsPage {
+//
+//    AndroidDriver driver;
+//
+//    public AlertsPage(AndroidDriver driver) {
+//        super();
+//        this.driver = driver;
+//        PageFactory.initElements(new AppiumFieldDecorator(driver), this);
+//    }
+//
+//
+//
+//    @AndroidFindBy(xpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.LinearLayout/android.view.ViewGroup/android.widget.TextView")
+//     private WebElement Alert_Title_text;
+//
+//    @AndroidFindBy(id = "com.spireon.fleet.staging:id/action_filter_alerts")
+//    private WebElement Filter_icon;
+//
+//    @AndroidFindBy(id = "com.spireon.fleet.staging:id/rv_filters")
+//    private WebElement Total_itemsCount;
+//
+//    @AndroidFindBy(id = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.LinearLayout/android.widget.RelativeLayout/android.view.ViewGroup[2]/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.RelativeLayout[2]")
+//    private WebElement Alert_List;
+//
+//    @AndroidFindBy(id = "com.android.permissioncontroller:id/permission_allow_foreground_only_button")
+//    private WebElement permission_access;
+//
+//    @AndroidFindBy(id = "com.spireon.fleet.staging:id/action_alerts")
+//    private WebElement Alerts_icon;
+//
+//    @AndroidFindBy(xpath = "(//android.view.View[@content-desc=\"MARKER_GEOZONE.\"])[17]")
+//    private WebElement Alert1;
+//
+//    @AndroidFindBy(xpath = "//android.widget.ImageButton[@content-desc=\"Navigate up\"]")
+//    private WebElement Back;
+//
+//    @AndroidFindBy(xpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.LinearLayout/android.view.ViewGroup[1]/android.widget.TextView")
+//    private WebElement Filters_Text;
+//
+//    @AndroidFindBy(id = "com.spireon.fleet.staging:id/snackbar_text")
+//    private WebElement Network_Error_Msg;
+//
+//    @AndroidFindBy(id = "com.spireon.fleet.staging:id/snackbar_action")
+//    private WebElement Retry;
+//    @AndroidFindBy(id = "com.spireon.fleet.staging:id/bb_bottom_bar_icon")
+//    private WebElement HomeScreen_icon;
+//
+//    public String safety="Safety (5)";
+//    public String prod="Productivity (4)";
+//    public String monitor="Monitoring (2)";
+//    public String asset="Asset Health (5)";
+//
+//
+//
+//
+//    public void Click_HomeScreen_icon()
+//    {
+//         HomeScreen_icon.click();
+//    }
+//
+//    public String safety1="Hard Braking and Hard Acceleration";
+//    public String safety2="Odd Hours";
+//    public String safety3="Posted Speed Limit";
+//    public String safety4="Speed Threshold";
+//    public String safety5="Unauthorized Movement";
+//
+//    public String productivity1="Idle";
+//    public String productivity2="Stop";
+//    public String productivity3="Input";
+//    public String productivity4="Landmark Arrival and Departure";
+//
+//    public String monitor1="Fuel Tank";
+//    public String monitor2="Temperature";
+//
+//    public String asset1="Low Battery";
+//    public String asset2="Maintenance";
+//    public String asset3="Tire Pressure";
+//    public String asset4="Engine Oil Life";
+//    public String asset5="DTC Alert";
+//
+//
+//    public String getNetwork_Error_Msg()
+//    {
+//       return  Network_Error_Msg.getText();
+//    }
+//
+//    public String network_msg="Please check your network connection and try again.";
+//
+//    public void ClickRetry()
+//    {
+//         Retry.click();
+//    }
+//    public String getFilter_text()
+//    {
+//        return Filters_Text.getText();
+//    }
+//
+//    public String filter_text="Filters";
+//
+//    public void ClickBack()
+//    {
+//        Back.click();
+//    }
+//    public void ClickAlert1()
+//    {
+//        Alert1.click();
+//    }
+//
+//    public void ClickAlerts_icon()
+//    {
+//        Alerts_icon.click();
+//    }
+//
+//
+//
+//    public String  getAlert_Title_text()
+//    {
+//        return Alert_Title_text.getText();
+//    }
+//    public String  Alert_Title="Alerts - 7 Day History";
+//
+//    public WebElement getFilter_icon()
+//    {
+//        return Filter_icon;
+//    }
+//
+//    public void Click_filter()
+//    {
+//        Filter_icon.click();
+//    }
+//
+//    public WebElement getAlert_List()
+//    {
+//        return Alert_List;
+//    }
+//
+//    public WebElement getTotal_itemsCount()
+//    {
+//        return Total_itemsCount;
+//    }
+//
+//    public void Click_Permission()
+//    {
+//        permission_access.click();
+//    }
+//
+//
+//}
--- src/test/java/AppiumAutomation/AlertsTestCases.java
+++ src/test/java/AppiumAutomation/AlertsTestCases.java
@@ -1,118 +1,118 @@
-package AppiumAutomation;
-
-import io.appium.java_client.AppiumBy;
-import io.appium.java_client.android.connection.ConnectionStateBuilder;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.testng.Assert;
-import org.testng.annotations.Test;
-
-import java.util.List;
-
-public class AlertsTestCases extends BaseClass
-{
-
-
-   // C21287  Verify Alerts screen UI
-    //C102181 Tap on any alert list item
-    //C147073 Tap on Filter icon
-    @Test(priority = 0)
-    public void testAlertsScreenUIVerification() throws InterruptedException {
-        signinpage.getSignIn().click();
-        signinpage.getUsername().sendKeys("Fleet360A");
-        signinpage.getPassword().sendKeys("Password@1");
-        signinpage.getSignIn().click();
-        alertsPage.Click_Permission();
-
-        alertsPage.ClickAlerts_icon();
-      //  Assert.assertTrue(alertsPage.getAlert_List().isDisplayed());
-
-
-        Assert.assertTrue(driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.LinearLayout/android.widget.RelativeLayout/android.view.ViewGroup[2]/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.RelativeLayout[2]")).isDisplayed());
-
-        Assert.assertEquals(alertsPage.getAlert_Title_text(),alertsPage.Alert_Title);
-        Assert.assertTrue(alertsPage.getTotal_itemsCount().isDisplayed());
-
-        List<WebElement> AlertList = driver.findElements(By.id("com.spireon.fleet.staging:id/ll_item"));
-        AlertList.get(0).click();
-        Thread.sleep(3000);
-        alertsPage.ClickBack();
-
-        Assert.assertTrue(alertsPage.getFilter_icon().isEnabled());
-        alertsPage.Click_filter();
-        Assert.assertEquals(alertsPage.getFilter_text(),alertsPage.filter_text);
-        alertsPage.ClickBack();
-
-
-    }
-
-
-    //C22123 Verify proper message is shown while loading alerts when network fails
-    //C22124 Tapping on retry option network call should happen
-    //C21297 Verify pull down to refresh functionality
-    @Test(priority = 1)
-    public void testAlertsScreenNetworkVerificaton() throws InterruptedException {
-
-        alertsPage.Click_HomeScreen_icon();
-        driver.setConnection(new ConnectionStateBuilder().withWiFiDisabled().withDataDisabled().build());
-        alertsPage.ClickAlerts_icon();
-        Thread.sleep(3000);
-        Assert.assertEquals(alertsPage.getNetwork_Error_Msg(),alertsPage.network_msg);
-        driver.setConnection(new ConnectionStateBuilder().withWiFiEnabled().withDataEnabled().build());
-        alertsPage.ClickRetry();
-        List<WebElement> AlertList = driver.findElements(By.id("com.spireon.fleet.staging:id/ll_item"));
-        Assert.assertFalse(AlertList.isEmpty());
-
-       Thread.sleep(3000);
-
-        pullToRefresh();
-
-    }
-
-    //C103327 Verify alert filter has categories like safety/Productivity/Monitoring/Asset health
-    @Test(priority = 2)
-    public void testAlertFilterCategoriesVerification() throws InterruptedException {
-        alertsPage.Click_filter();
-        Thread.sleep(2000);
-        List<WebElement> FilterCategoriesList = driver.findElements(By.id("com.spireon.fleet.staging:id/tv_filter_type"));
-        Assert.assertTrue(FilterCategoriesList.get(0).getText().contains(alertsPage.safety));
-        Assert.assertTrue(FilterCategoriesList.get(1).getText().contains(alertsPage.prod));
-        Assert.assertTrue(FilterCategoriesList.get(2).getText().contains(alertsPage.monitor));
-        Thread.sleep(2000);
-        String Asset_health = driver.findElement(AppiumBy.androidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"Asset Health (5)\"));")).getText();
-        Assert.assertTrue(Asset_health.contains(alertsPage.asset));
-        alertsPage.ClickBack();
-
-    }
-
-
-    //C103323 Verify Safety categories in Alerts Filter screen
-    //C103324 Verify Productivity categories in Alerts Filter screen
-    //C103325 Verify Monitoring categories in Alerts Filter screen
-    //C103326 Verify Asset health categories in Alerts Filter screen
-    @Test(priority = 3)
-    public void testSafetyFilterVerfication() throws InterruptedException {
-
-        alertsPage.Click_filter();
-        Thread.sleep(2000);
-        List<WebElement> FilterList = driver.findElements(By.id("com.spireon.fleet.staging:id/material_drawer_name"));
-        Assert.assertTrue(FilterList.get(0).getText().contains(alertsPage.safety1));
-        Assert.assertTrue(FilterList.get(1).getText().contains(alertsPage.safety2));
-        Assert.assertTrue(FilterList.get(2).getText().contains(alertsPage.safety3));
-        Assert.assertTrue(FilterList.get(3).getText().contains(alertsPage.safety4));
-        Assert.assertTrue(FilterList.get(4).getText().contains(alertsPage.safety5));
-        Assert.assertTrue(FilterList.get(5).getText().contains(alertsPage.productivity1));
-        Assert.assertTrue(FilterList.get(6).getText().contains(alertsPage.productivity2));
-        Assert.assertTrue(FilterList.get(7).getText().contains(alertsPage.productivity3));
-        Assert.assertTrue(FilterList.get(8).getText().contains(alertsPage.productivity4));
-        Assert.assertTrue(FilterList.get(9).getText().contains(alertsPage.monitor1));
-        Assert.assertTrue(FilterList.get(10).getText().contains(alertsPage.monitor2));
-        Assert.assertEquals( driver.findElement(AppiumBy.androidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"Low Battery\"));")).getText(),alertsPage.asset1);
-        Assert.assertEquals( driver.findElement(AppiumBy.androidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"Maintenance\"));")).getText(),alertsPage.asset2);
-        Assert.assertEquals( driver.findElement(AppiumBy.androidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"Tire Pressure\"));")).getText(),alertsPage.asset3);
-        Assert.assertEquals( driver.findElement(AppiumBy.androidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"Engine Oil Life\"));")).getText(),alertsPage.asset4);
-        Assert.assertEquals( driver.findElement(AppiumBy.androidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"DTC Alert\"));")).getText(),alertsPage.asset5);
-
-    }
-
-}
+//package AppiumAutomation;
+//
+//import io.appium.java_client.AppiumBy;
+//import io.appium.java_client.android.connection.ConnectionStateBuilder;
+//import org.openqa.selenium.By;
+//import org.openqa.selenium.WebElement;
+//import org.testng.Assert;
+//import org.testng.annotations.Test;
+//
+//import java.util.List;
+//
+//public class AlertsTestCases extends BaseClass
+//{
+//
+//
+//   // C21287  Verify Alerts screen UI
+//    //C102181 Tap on any alert list item
+//    //C147073 Tap on Filter icon
+//    @Test(priority = 0)
+//    public void testAlertsScreenUIVerification() throws InterruptedException {
+//        signinpage.getSignIn().click();
+//        signinpage.getUsername().sendKeys("Fleet360A");
+//        signinpage.getPassword().sendKeys("Password@1");
+//        signinpage.getSignIn().click();
+//        alertsPage.Click_Permission();
+//
+//        alertsPage.ClickAlerts_icon();
+//      //  Assert.assertTrue(alertsPage.getAlert_List().isDisplayed());
+//
+//
+//        Assert.assertTrue(driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.LinearLayout/android.widget.RelativeLayout/android.view.ViewGroup[2]/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.RelativeLayout[2]")).isDisplayed());
+//
+//        Assert.assertEquals(alertsPage.getAlert_Title_text(),alertsPage.Alert_Title);
+//        Assert.assertTrue(alertsPage.getTotal_itemsCount().isDisplayed());
+//
+//        List<WebElement> AlertList = driver.findElements(By.id("com.spireon.fleet.staging:id/ll_item"));
+//        AlertList.get(0).click();
+//        Thread.sleep(3000);
+//        alertsPage.ClickBack();
+//
+//        Assert.assertTrue(alertsPage.getFilter_icon().isEnabled());
+//        alertsPage.Click_filter();
+//        Assert.assertEquals(alertsPage.getFilter_text(),alertsPage.filter_text);
+//        alertsPage.ClickBack();
+//
+//
+//    }
+//
+//
+//    //C22123 Verify proper message is shown while loading alerts when network fails
+//    //C22124 Tapping on retry option network call should happen
+//    //C21297 Verify pull down to refresh functionality
+//    @Test(priority = 1)
+//    public void testAlertsScreenNetworkVerificaton() throws InterruptedException {
+//
+//        alertsPage.Click_HomeScreen_icon();
+//        driver.setConnection(new ConnectionStateBuilder().withWiFiDisabled().withDataDisabled().build());
+//        alertsPage.ClickAlerts_icon();
+//        Thread.sleep(3000);
+//        Assert.assertEquals(alertsPage.getNetwork_Error_Msg(),alertsPage.network_msg);
+//        driver.setConnection(new ConnectionStateBuilder().withWiFiEnabled().withDataEnabled().build());
+//        alertsPage.ClickRetry();
+//        List<WebElement> AlertList = driver.findElements(By.id("com.spireon.fleet.staging:id/ll_item"));
+//        Assert.assertFalse(AlertList.isEmpty());
+//
+//       Thread.sleep(3000);
+//
+//        pullToRefresh();
+//
+//    }
+//
+//    //C103327 Verify alert filter has categories like safety/Productivity/Monitoring/Asset health
+//    @Test(priority = 2)
+//    public void testAlertFilterCategoriesVerification() throws InterruptedException {
+//        alertsPage.Click_filter();
+//        Thread.sleep(2000);
+//        List<WebElement> FilterCategoriesList = driver.findElements(By.id("com.spireon.fleet.staging:id/tv_filter_type"));
+//        Assert.assertTrue(FilterCategoriesList.get(0).getText().contains(alertsPage.safety));
+//        Assert.assertTrue(FilterCategoriesList.get(1).getText().contains(alertsPage.prod));
+//        Assert.assertTrue(FilterCategoriesList.get(2).getText().contains(alertsPage.monitor));
+//        Thread.sleep(2000);
+//        String Asset_health = driver.findElement(AppiumBy.androidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"Asset Health (5)\"));")).getText();
+//        Assert.assertTrue(Asset_health.contains(alertsPage.asset));
+//        alertsPage.ClickBack();
+//
+//    }
+//
+//
+//    //C103323 Verify Safety categories in Alerts Filter screen
+//    //C103324 Verify Productivity categories in Alerts Filter screen
+//    //C103325 Verify Monitoring categories in Alerts Filter screen
+//    //C103326 Verify Asset health categories in Alerts Filter screen
+//    @Test(priority = 3)
+//    public void testSafetyFilterVerfication() throws InterruptedException {
+//
+//        alertsPage.Click_filter();
+//        Thread.sleep(2000);
+//        List<WebElement> FilterList = driver.findElements(By.id("com.spireon.fleet.staging:id/material_drawer_name"));
+//        Assert.assertTrue(FilterList.get(0).getText().contains(alertsPage.safety1));
+//        Assert.assertTrue(FilterList.get(1).getText().contains(alertsPage.safety2));
+//        Assert.assertTrue(FilterList.get(2).getText().contains(alertsPage.safety3));
+//        Assert.assertTrue(FilterList.get(3).getText().contains(alertsPage.safety4));
+//        Assert.assertTrue(FilterList.get(4).getText().contains(alertsPage.safety5));
+//        Assert.assertTrue(FilterList.get(5).getText().contains(alertsPage.productivity1));
+//        Assert.assertTrue(FilterList.get(6).getText().contains(alertsPage.productivity2));
+//        Assert.assertTrue(FilterList.get(7).getText().contains(alertsPage.productivity3));
+//        Assert.assertTrue(FilterList.get(8).getText().contains(alertsPage.productivity4));
+//        Assert.assertTrue(FilterList.get(9).getText().contains(alertsPage.monitor1));
+//        Assert.assertTrue(FilterList.get(10).getText().contains(alertsPage.monitor2));
+//        Assert.assertEquals( driver.findElement(AppiumBy.androidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"Low Battery\"));")).getText(),alertsPage.asset1);
+//        Assert.assertEquals( driver.findElement(AppiumBy.androidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"Maintenance\"));")).getText(),alertsPage.asset2);
+//        Assert.assertEquals( driver.findElement(AppiumBy.androidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"Tire Pressure\"));")).getText(),alertsPage.asset3);
+//        Assert.assertEquals( driver.findElement(AppiumBy.androidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"Engine Oil Life\"));")).getText(),alertsPage.asset4);
+//        Assert.assertEquals( driver.findElement(AppiumBy.androidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"DTC Alert\"));")).getText(),alertsPage.asset5);
+//
+//    }
+//
+//}
